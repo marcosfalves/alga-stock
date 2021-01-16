a4b914af8ae17793039ba7299fe931e564dddbad
marcosfalves <marcosf_alves@outlook.com>
2021-01-16 11:49:50 -0300

    created input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import TestComponent from '../TestComponent'
-import ClassComponent from '../ClassComponent'
+import Header from '../Header';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
+import Input from '../../shared/Input';
+
+function TestComponent () {
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
 
 function App() {
+  const [street, setStreet] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent />
-        <ClassComponent name="Marcos" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header title="AlgaStock" />
+
+      <Container>
+        <Button
+          onClick={() => window.alert('UIIIU')}
+          appendIcon={<TestComponent />}
         >
-          Learn React
-        </a>
-      </header>
+          Alert
+        </Button>
+
+        <Input 
+          label="Street"
+          placeholder="15th Avenue"
+          value={street}
+          onChange={e => setStreet(e.target.value)}
+          />
+      </Container>
     </div>
   );
 }
